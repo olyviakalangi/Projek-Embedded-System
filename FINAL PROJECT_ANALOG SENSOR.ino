b9f8e53367b90cf761cc9ddde6ce9d4f86088970
olyviakalangi <76098113+olyviakalangi@users.noreply.github.com>
2022-05-23 17:30:23 +0700

    Update FINAL PROJECT_ANALOG SENSOR.ino

--- FINAL PROJECT_ANALOG SENSOR.ino
+++ FINAL PROJECT_ANALOG SENSOR.ino
@@ -12,35 +12,50 @@ void setup() {
 
 void loop() 
 {
-  int dataAnalog = analogRead (A0);  //inisialisasi dataAnalog sebagai baca sensor pada pin analog
-  Serial.println(dataAnalog);
+  int dataAnalog = analogRead (A0);  //inisialisasi dataAnalog sebagai baca sensor pada pin analog A0
+  long nilaiTerendah  = 0;          
+  long nilaiTertinggi = 1023;        
+  long persentase;
+  
+  Serial.println(dataAnalog);       //Jika ingin melihat melalui serial monitor
+  persentase = map(dataAnalog, nilaiTerendah, nilaiTertinggi, 0, 100);  //Membuat nilai analog menjadi digital
+  Serial.println(persentase);
+ 
+  if(dataAnalog>=870)               //Relay Digital (2 output: ON/OFF; Indikator 0 = Basah dan 1 = Kering
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Kekeringan: ");
+    lcd.print(persentase);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("= Media Kering =");
+    digitalWrite(pompa,1);
+  }
+  else if(dataAnalog>=500 && dataAnalog<=850)          //Relay Digital (2 output: ON/OFF; Indikator 0 = Basah dan 1 = Kering
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Kekeringan: ");
+    lcd.print(persentase);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("= Media Medium =");
+    digitalWrite(pompa,0);
+  }
+  else if(dataAnalog<=450)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Kekeringan: ");
+    lcd.print(persentase);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("= Media  Basah =");
+    digitalWrite(pompa,0);
+  }
+  delay(5000);
   lcd.clear();
   lcd.setCursor(4,0);
   lcd.print("Kelompok 4");
   lcd.setCursor(0,1);
-  lcd.println("Embedded System ");
+  lcd.print("Embedded System ");
   delay(1000);
- 
-  //lcd.setCursor(2,0);
-  //lcd.print("Status Media");
-  
-  if(dataAnalog>=900)          //Relay Digital (2 output: ON/OFF; Indikator 0 = Basah dan 1 = Kering
-  {
-    lcd.setCursor(0,1);
-    lcd.print(" Media Kering  ");
-    digitalWrite(pompa,1);
-  }
-  if(dataAnalog>=500 && dataAnalog<=850)          //Relay Digital (2 output: ON/OFF; Indikator 0 = Basah dan 1 = Kering
-  {
-    lcd.setCursor(0,1);
-    lcd.print(" Media Medium  ");
-    digitalWrite(pompa,0);
-  }
-  if(dataAnalog<=450)
-  {
-    lcd.setCursor(0,1);
-    lcd.print("   Media basah  ");
-    digitalWrite(pompa,0);
-  }
-  delay(5000);
 }
